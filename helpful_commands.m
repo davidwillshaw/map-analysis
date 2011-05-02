a00bdaeaef070729f66438d449b46fc91e85cbd9
Adrianna Teriakidis <s0091333@jupiter2.inf.ed.ac.uk>
2011-05-02 21:14:05 +0100

    changing the subgraph algorithm to include connectivity constraint.

--- helpful_commands.m
+++ helpful_commands.m
@@ -6,10 +6,16 @@ params = getparams(id)
    params = select_point_positions(params,'FTOC')
    params = create_projection(params, 'CTOF')
    params = create_projection(params, 'FTOC')
+    params = triangulation(params,'CTOF')
+     params = triangulation(params,'FTOC')
+     params = find_crossings(params, 'CTOF')
+     params = find_crossings(params, 'FTOC')
+     
 
    imagesc(params.elev_amp)
 hold on
 >>  plot(params.CTOF.coll_points(:,1),params.CTOF.coll_points(:,2),'xk')
+hold on
 plot(params.FTOC.coll_points(:,1),params.FTOC.coll_points(:,2),'xm')
 >>  figure
 plot(params.FTOC.field_points(:,1),params.FTOC.field_points(:,2),'xm')
