b89a97ee16df1e611c5c2853bb4f901110760260
David C Sterratt <david.c.sterratt@ed.ac.uk>
2013-02-27 16:33:09 +0000

    Add SEM dispersion variables to output

--- find_dispersion.m
+++ find_dispersion.m
@@ -27,6 +27,14 @@ function params = find_dispersion(params, direction)
 %       of ellipses fitted to complementary distributions, referred
 %       to as SDW2 in Willshaw et al. (2013), Table 3.
 %
+%     params.stats.FTOC.SEM_xrad - Mean length of major axes of
+%       ellipses fitted to standard error in mean of complementary
+%       distributions.
+%
+%     params.stats.FTOC.SEM_yrad - Mean length of minor axes of
+%       ellipses fitted to standard error in mean of complementary
+%       distributions.
+%
 % See also plot_figure5, get_complmentary_ellipses
 
 [x_cent_from, y_cent_from, angle_from, x_radius_from, y_radius_from, ...
@@ -42,6 +50,21 @@ if strcmp(direction,'FTOC')
     params.stats.FTOC.dispersion_xrad = mean(x_radius_to);
     params.stats.FTOC.dispersion_yrad = mean(y_radius_to);
 end
+
+[x_cent_from, y_cent_from, angle_from, x_radius_from, y_radius_from, ...
+ x_cent_to,   y_cent_to,   angle_to,   x_radius_to,   y_radius_to] = ...
+    get_complementary_ellipses(params, direction, 'sem');
+
+if strcmp(direction,'CTOF')
+    params.stats.CTOF.SEM_xrad = mean(x_radius_to);
+    params.stats.CTOF.SEM_yrad = mean(y_radius_to);
+end
+   
+if strcmp(direction,'FTOC')
+    params.stats.FTOC.SEM_xrad = mean(x_radius_to);
+    params.stats.FTOC.SEM_yrad = mean(y_radius_to);
+end
+
 
 % Local Variables:
 % matlab-indent-level: 4
