758c01bd09db6ded09cfd226bf205d866782542c
David C Sterratt <david.c.sterratt@ed.ac.uk>
2013-03-01 16:15:01 +0000

    Make output quiet

--- csvwritestruct.m
+++ csvwritestruct.m
@@ -3,43 +3,48 @@ function csvwritestruct(filename, s)
 %   Write the structure S to a csv file FILENAME
 %       
     s1 = unfold_structure(s);
-    els = structfun(@format_element, s1, 'UniformOutput', false)
-    cells = struct2cell(els)
-    names = fieldnames(s1)
+    els = structfun(@format_element, s1, 'UniformOutput', false);
+    cells = struct2cell(els);
+    names = fieldnames(s1);
     if (length(cells) ~= length(names)) 
         error('Names and cells are different lengths')
     end
     f = fopen(filename, 'w');
     for (i = 1:length(names)) 
-        fprintf(f, '"%s"', names{i})
+        fprintf(f, '"%s"', names{i});
         if (i ~= length(names))
-            fprintf(f, ',')
+            fprintf(f, ',');
         else                    
-            fprintf(f, '\n')
+            fprintf(f, '\n');
         end
     end
     for (i = 1:length(names)) 
-        fprintf(f, '%s', cells{i})
+        fprintf(f, '%s', cells{i});
         if (i ~= length(names))
-            fprintf(f, ',')
+            fprintf(f, ',');
         else                    
-            fprintf(f, '\n')
+            fprintf(f, '\n');
         end
     end
-    fclose(f)
+    fclose(f);
     
 end
 
 function out =  format_element(el) 
     if (isstr(el))
-        out = sprintf('"%s"', el)
-    else
+        out = sprintf('"%s"', el);
+        return
+    end
+    if (isnumeric(el))
         if (prod(size(el)) == 1)
-            out = sprintf('%f', el)
+            out = sprintf('%f', el);
         else
             warning('Matrix or vector: outputting NA')
-            out = sprintf('"NA"', el)
+            out = sprintf('"NA"');
         end
+        return
     end
+    warning('Invalid format: outputting NA')
+    out = sprintf('"NA"');
 end   
     
