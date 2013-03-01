3f782e85a6de3993a0e94079dcfd19d371587a20
David C Sterratt <david.c.sterratt@ed.ac.uk>
2013-03-01 17:23:41 +0000

    First attempt at writing multiple rows

--- csvwritestruct.m
+++ csvwritestruct.m
@@ -2,32 +2,43 @@ function csvwritestruct(filename, s)
 % CSVWRITESTRUCT - write a structure to a csv file
 %   Write the structure S to a csv file FILENAME
 %       
-    s1 = unfold_structure(s);
-    els = structfun(@format_element, s1, 'UniformOutput', false);
-    cells = struct2cell(els);
-    names = fieldnames(s1);
-    if (length(cells) ~= length(names)) 
-        error('Names and cells are different lengths')
-    end
-    f = fopen(filename, 'w');
-    for (i = 1:length(names)) 
-        fprintf(f, '"%s"', names{i});
-        if (i ~= length(names))
-            fprintf(f, ',');
-        else                    
-            fprintf(f, '\n');
+    if (iscell(s)) 
+        f = fopen(filename, 'w');
+        for (k = 1:length(s))
+            s1 = unfold_structure(s{k});
+            els = structfun(@format_element, s1, 'UniformOutput', false);
+            cells = struct2cell(els);
+            if (k == 1) 
+                names = fieldnames(s1);
+            else
+                if (length(fieldnames(s1)) ~= length(names))
+                    error('Two structures have different numbers of elements')
+                end
+            end
+            if (length(cells) ~= length(names)) 
+                error('Names and cells are different lengths')
+            end
+            if (k == 1) 
+                for (i = 1:length(names)) 
+                    fprintf(f, '"%s"', names{i});
+                    if (i ~= length(names))
+                        fprintf(f, ',');
+                    else                    
+                        fprintf(f, '\n');
+                    end
+                end
+            end
+            for (i = 1:length(names)) 
+                fprintf(f, '%s', cells{i});
+                if (i ~= length(names))
+                    fprintf(f, ',');
+                else                    
+                    fprintf(f, '\n');
+                end
+            end
         end
+        fclose(f);
     end
-    for (i = 1:length(names)) 
-        fprintf(f, '%s', cells{i});
-        if (i ~= length(names))
-            fprintf(f, ',');
-        else                    
-            fprintf(f, '\n');
-        end
-    end
-    fclose(f);
-    
 end
 
 function out =  format_element(el) 
