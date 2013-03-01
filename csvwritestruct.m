120aac6696a4e0b79eef5bf82f6b49ffe5e1aec8
David C Sterratt <david.c.sterratt@ed.ac.uk>
2013-03-01 13:59:02 +0000

    Initial revision of csvwritestruct

---
+++ csvwritestruct.m
@@ -0,0 +1,45 @@
+function csvwritestruct(filename, s)
+% CSVWRITESTRUCT - write a structure to a csv file
+%   Write the structure S to a csv file FILENAME
+%       
+    s1 = unfold_structure(s);
+    els = structfun(@format_element, s1, 'UniformOutput', false)
+    cells = struct2cell(els)
+    names = fieldnames(s1)
+    if (length(cells) ~= length(names)) 
+        error('Names and cells are different lengths')
+    end
+    f = fopen(filename, 'w');
+    for (i = 1:length(names)) 
+        fprintf(f, '"%s"', names{i})
+        if (i ~= length(names))
+            fprintf(f, ',')
+        else                    
+            fprintf(f, '\n')
+        end
+    end
+    for (i = 1:length(names)) 
+        fprintf(f, '%s', cells{i})
+        if (i ~= length(names))
+            fprintf(f, ',')
+        else                    
+            fprintf(f, '\n')
+        end
+    end
+    fclose(f)
+    
+end
+
+function out =  format_element(el) 
+    if (isstr(el))
+        out = sprintf('"%s"', el)
+    else
+        if (prod(size(el)) == 1)
+            out = sprintf('%f', el)
+        else
+            warning('Matrix or vector: outputting NA')
+            out = sprintf('"NA"', el)
+        end
+    end
+end   
+    
